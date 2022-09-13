0359f1d14cf414c275f37cd301eb20bb50a15edc
Xiao <xmeng1@ualberta.ca>
2022-09-12 22:05:06 -0600

    Set up boilerplate for pages

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
+
 import App from './App';
-import store from './redux/store';
+// import store from './redux/store';
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  // <Provider store={store}>
+  <App />
+  // </Provider>
 );
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Login from '../components/Login';
+import Dashboard from '../components/Dashboard';
+// import styles from '../styles/home.module.css';
+
+// Check if cookie if cookie in state, if yes then render Dashboard, if no then render login.
+export default function Home() {
+  const userInState = true;
+
+  return userInState ? <Dashboard /> : <Login />;
+}
